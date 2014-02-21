70d41405388f03898bb551815796461de7a9eab8
Maksim Stepanov <stepanov.ms@mail.ru>
2014-02-21 12:14:27 +0400

    Added progress bar for reload indication in dumb browsers

--- README.md
+++ README.md
@@ -14,6 +14,24 @@ $('a').on 'click', IFrameReloader.click
 
 And that's it.
 
+
+# Progress bar
+
+This can be used with custom progressbar, cause mobile browsers not showing it's progress. It can be used automatically with *useProgressBar* option is set. Or can be used as it's own.
+
+Usage:
+```javascript
+// shows progress bar with progress 0 is does nothing if progress exists
+IFrameReloader.showProgress();
+
+// showing progress bar if not, and sets progress to *value*, value is between 0 and 1.
+IFrameReloader.setProgress(value);
+
+// hides progress with effect of 100% done
+IFrameReloader.hideProgress();
+```
+Configuration for this a little bit lower.
+
 # Configuration
 
 ```javascipt
@@ -24,6 +42,12 @@ IFrameReloader.configure({
     iframeId: 'util-iframe',
     onload: function() { console.log('success'); },
     onerror: function() { console.log('error or abort'); }
+    useProgressBar: true,
+    progressBarId: 'ifr-progress',
+    progressInnerBarClass: 'indicator',
+    progressAppendTo: 'body',
+    progressRemoveTimeout: 200,
+    progressHideOnMaxValueSet: true
 });
 ```
 
@@ -37,4 +61,10 @@ replacementContainerAttribute | Attribute to change content of successfully load
 iframeId | id of created iframe if needed
 onload | Callback on successfully loaded page
 onerror | Callback on error or abort loading page
+useProgressBar| Set true to use custom progressbar, useful on mobile devices, which not showing browser progress
+progressBarId| id of created progressbar div
+progressInnerBarClass| Class is created progressbar indicator inside progressbar div, for styling purposes
+progressAppendTo| Query string of element is append to
+progressRemoveTimeout| Timeout of progressbar removal when it's done for animation purposes
+progressHideOnMaxValueSet| true is for automatically hide when max value set.
 
--- iframe_reloader.js
+++ iframe_reloader.js
@@ -1,17 +1,28 @@
 (function (ifr) {
   'use strict';
 
+//  =========================== Variables
+
   var opts = {
     urlParam: '_ifr',
     dataUrlAttribute: 'data-ifr-url',
-    replacementContainerAttribute: 'data-pjax-container',
+    replacementContainerAttribute: 'data-ifr-container',
     iframeId: 'util-iframe',
     onload: null,
-    onerror: null
+    onerror: null,
+    useProgressBar: true,
+    progressBarId: 'ifr-progress',
+    progressInnerBarClass: 'indicator',
+    progressAppendTo: 'body',
+    progressRemoveTimeout: 200,
+    progressHideOnMaxValueSet: true
   },
     support = {
       historyApi: !!(window.history && window.history.pushState)
-    };
+    },
+    progressBar, innerBar, fakeWorkTimer, currentProgress = 0;
+
+//  =========================== Utils
 
   function isFunction(func) {
     var getType = {};
@@ -27,6 +38,94 @@
     }
     return result;
   }
+
+  function bindTransitionendEvent(elem, callback) {
+    var event_names = ['transitionend', 'webkitTransitionEnd', 'oTransitionEnd', 'MSTransitionEnd'], i;
+    for (i = 0; i < event_names.length; i++) {
+      elem.addEventListener(event_names[i], callback, false);
+    }
+  }
+
+  function toLimits(value, min, max) {
+    if (value > max) { return max; }
+    if (value < min) { return min; }
+    return value;
+  }
+
+//  =========================== Private methods
+
+  function progressStopFakeWork() {
+    clearTimeout(fakeWorkTimer);
+  }
+
+  function insertProgress(progress) {
+    if (progressBar) { return; }
+
+    progress = progress || 0;
+    progress = toLimits(progress);
+
+
+    progressBar = document.createElement('div');
+    progressBar.id = opts.progressBarId;
+    document.querySelector(opts.progressAppendTo).appendChild(progressBar);
+
+    innerBar = document.createElement('div');
+    innerBar.className = opts.progressInnerBarClass;
+    innerBar.style.width = (progress * 100) + '%';
+    innerBar.style.height = '100%';
+    progressBar.appendChild(innerBar);
+    currentProgress = progress;
+  }
+
+  function removeProgress() {
+    if (progressBar) {
+      progressBar.remove();
+      progressBar = innerBar = null;
+      currentProgress = 0;
+    }
+  }
+
+  function hideProgress() {
+    if (progressBar) {
+      progressStopFakeWork();
+      innerBar.style.width = '100%';
+      bindTransitionendEvent(innerBar, removeProgress);
+      setTimeout(removeProgress, opts.progressRemoveTimeout);
+      innerBar.style.display = 'hidden';
+
+    }
+  }
+
+  function setProgress(progress) {
+    progress = toLimits(progress);
+    if (progressBar) {
+      innerBar.style.width = progress * 100 + '%';
+      currentProgress = progress;
+
+      if (opts.progressHideOnMaxValueSet && progress >= 1) {
+        hideProgress();
+      }
+    } else {
+      insertProgress(progress);
+    }
+  }
+
+  function progressStartFakeWork() {
+    var work = function () {
+      setProgress(currentProgress + Math.random() * 0.05);
+      fakeWorkTimer = setTimeout(work, 300);
+    };
+    work();
+  }
+
+
+  function onerrorHandler(event) {
+    event.preventDefault();
+    event.currentTarget.remove();
+    if (isFunction(opts.onerror)) { opts.onerror(event); }
+    hideProgress();
+  }
+
 
   function onloadHandler(event) {
     event.preventDefault();
@@ -52,18 +151,18 @@
       source = innerDoc.querySelector('[' + opts.replacementContainerAttribute + ']'),
       target = document.querySelector('[' + opts.replacementContainerAttribute + ']');
 
+    if (source ===  null) {
+      onerrorHandler(event);
+      return;
+    }
+
     history.pushState(null, document.title, removeIFRParameter(innerDoc.location.href));
     target.innerHTML = source.innerHTML;
     document.title = innerDoc.title;
 
     iframe.remove();
     if (isFunction(opts.onload)) { opts.onload(event); }
-  }
-
-  function onerrorHandler(event) {
-    event.preventDefault();
-    event.currentTarget.remove();
-    if (isFunction(opts.onerror)) { opts.onerror(event); }
+    hideProgress();
   }
 
   function requestPage(url) {
@@ -84,6 +183,9 @@
     requestPage(window.location.href);
   }
 
+
+//  =========================== Public methods
+
   ifr.configure = function (options) {
     opts = extend(options, opts);
   };
@@ -93,6 +195,11 @@
       return;
     }
     event.preventDefault();
+
+    if (opts.useProgressBar) {
+      setProgress(0);
+      progressStartFakeWork();
+    }
 
     var element = event.currentTarget,
       url = element.hasOwnProperty(opts.dataUrlAttribute) ?
@@ -111,6 +218,12 @@
 
     requestPage(url);
   };
+
+  ifr.setProgress = setProgress;
+  ifr.showProgress = insertProgress;
+  ifr.hideProgress = hideProgress;
+
+//  =========================== Initialization
 
   function init() {
     window.addEventListener('popstate', popstateHandler, false);
